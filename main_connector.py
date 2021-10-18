a71005da75f887f6ea9fbe81007c10e3762d7ae1
theS-K-B <mailskbat@gmail.com>
2021-10-18 13:50:31 +0530

    major bugs related to primary key are fixed

--- main_connector.py
+++ main_connector.py
@@ -11,16 +11,14 @@ country_list = df['Country'].unique()
 
 # for each country create a diffrent table in database
 # e.g. table_ind, table_au
-# for country in country_list:
-#     df.loc[df['Country'] == country].to_sql(
-#     name='table_'+country.lower(),
-#     con=engine,
-#     index=False,
-#     dtype={'Customer_Name':VARCHAR(255)},
-#     if_exists='replace')
-#     engine.execute(f'ALTER TABLE `table_{country.lower()}` ADD PRIMARY KEY (`Customer_Name`);')
-
-
+for country in country_list:
+    df.loc[df['Country'] == country].to_sql(
+    name='table_'+country.lower(),
+    con=engine,
+    index=False,
+    dtype={'Customer_Name':VARCHAR(255)},
+    if_exists='replace')
+    engine.execute(f'ALTER TABLE `table_{country.lower()}` ADD PRIMARY KEY (`Customer_Name`);')
 
 # get the data from specified country table
 def get_datafile(country):
