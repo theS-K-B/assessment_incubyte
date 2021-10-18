c7b2be126ba547cb4298ccee7f1e8d52a0006904
theS-K-B <mailskbat@gmail.com>
2021-10-18 11:51:29 +0530

    country specific tables added to database

--- main_connector.py
+++ main_connector.py
@@ -7,3 +7,12 @@ df = pd.read_sql_table('customers', engine, index_col='Customer_Name')
 
 # list of unique countries
 country_list = df['Country'].unique()
+
+# for each country create a diffrent table in database
+# e.g. table_ind, table_au
+for country in country_list:
+    df.loc[df['Country'] == country].to_sql(
+    name='table_'+country,
+    con=engine,
+    index=False,
+    if_exists='replace')
