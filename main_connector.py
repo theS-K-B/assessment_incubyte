0c07ebf17b8107455c1a8b214c2156807ad0ec5b
theS-K-B <mailskbat@gmail.com>
2021-10-18 11:37:14 +0530

    retrive the list of unique counties from database

--- main_connector.py
+++ main_connector.py
@@ -5,4 +5,5 @@ import sqlalchemy as sa
 engine = sa.create_engine('mysql+pymysql://root:12345@localhost:3306/cust_db')
 df = pd.read_sql_table('customers', engine, index_col='Customer_Name')
 
-print(df)
+# list of unique countries
+country_list = df['Country'].unique()
